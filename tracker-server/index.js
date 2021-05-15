67118e4266bc2c3889dd2f43baffb575dfe5fc56
Amine <aminefilali@outlook.dk>
2021-05-15 10:04:58 +0200

    tracks added

--- tracker-server/index.js
+++ tracker-server/index.js
@@ -1,19 +1,21 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const requireAuth = require('./middlewares/requireAuth');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
 const authRoutes = require('./routes/auth-routes');
+const trackRoutes = require('./routes/track-routes');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -26,8 +28,8 @@ mongoose.connection.on('error', (err) => {
   console.error('Error connecting to MongoDB', err);
 });
 
-app.get('/', requireAuth, (req, res) => {
-  res.send(`Welcome ${req.user.email}`);
+app.get('/', (req, res) => {
+  res.send('Tracker App');
 });
 
 app.listen(3000, () => {
